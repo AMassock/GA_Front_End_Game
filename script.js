c12cdadd9016f19942aa3cfc1723a2e814a53e34
amassock <ajmassock@gmail.com>
2023-01-07 20:53:09 -0600

    add reset listener and add personalized greeting

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-let startBtn = document.getElementById('start');
+let subUsr = document.getElementById('usr');
 let form = document.querySelector('form');
 let ans = document.querySelector('.answers');
 let subBtn = document.querySelector('.submit');
-let questSection = document.querySelector('.questions')
+let questSection = document.querySelector('.questions');
+let hello = document.querySelector('.hello');
+let startBtn = document.getElementById('start');
+let usr = localStorage.getItem('input');
+let reset = document.getElementById('reset');
 
-startBtn.addEventListener('click', function(e) {
+subUsr.addEventListener('click', function(e) {
     e.preventDefault();
     form.style.display = 'none';
+    document.getElementById('greet').innerHTML = `Hello ${usr}! Are you ready to begin your Pokémon journey?`;
+    hello.style.display = 'inline';
+    returnUsr(usr);
+});
+
+startBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    hello.style.display = 'none';
     questSection.style.display = 'block';
     ans.style.display = 'grid';
     subBtn.style.display = 'inline';
+});
+
+reset.addEventListener('click', function(e) {
+    e.preventDefault();
+    form.style.setProperty('display', 'initial');
+    hello.style.display = 'none';
 })
 
 let userAnswer = '';
@@ -26,3 +44,8 @@ let myQuestions = [{
     correct: 'a'
 }]
 
+function returnUsr() {
+    usr = document.querySelector('input').value;
+    localStorage.setItem('input',usr)
+    console.log(usr);
+}
